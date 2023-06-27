7aec21885041da3700f4c87205d96b9b0af08a9d
sandhya1301 <sandhyashetty.1813@gmail.com>
2023-06-27 21:49:49 +0530

    train info

---
+++ Train.java
@@ -0,0 +1,21 @@
+class Train{
+	public static void main(String[] args){
+		//int[] a={12267,12268,22201,22204,12426};
+		int train1=12267;
+		int train2=12268;
+		int train3=22201;
+		int train4=22204;
+		int train5=12426;
+		
+		int[] trains={train1,train2,train3,train4,train5,65431};
+		
+		int a=trains.length;
+		System.out.println(a);
+		System.out.println(trains[0]);
+		System.out.println(trains[1]);
+		System.out.println(trains[2]);
+		System.out.println(trains[3]);
+		System.out.println(trains[4]);
+		System.out.println(trains[5]);
+	}
+}
