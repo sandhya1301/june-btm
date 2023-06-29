29316d7cae2ed2d7d9ad7090bab8eb7874609437
sandhya1301 <sandhyashetty.1813@gmail>
2023-06-29 13:14:52 +0530

    hello

---
+++ WatchRunner.java
@@ -0,0 +1,18 @@
+class WatchRunner{
+	public static void main(String[] things)
+	{
+		System.out.println("running main class");
+		Watch watch=new Watch();
+		Watch brand=new Watch();
+		Watch price=new Watch();
+		Watch color=new Watch();
+		Watch manufacturedate=new Watch();
+		Watch types=new Watch();
+		Watch time=new Watch();
+		Watch warrenty=new Watch();
+		Watch inventor=new Watch();
+		Watch help=new Watch();
+		Watch digits=new Watch();
+	}
+	}
+		
