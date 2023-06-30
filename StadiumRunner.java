76fe1132ec89c23f6962bbea1d9d1ce0fccbb870
sandhya1301 <sandhyhc.xworkz@gmail.com>
2023-06-30 15:19:02 +0530

    stadiumRunner

---
+++ StadiumRunner.java
@@ -0,0 +1,17 @@
+class StadiumRunner
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Running main");
+		
+		Stadium stadium=new Stadium();
+		Stadium name=new Stadium();
+		Stadium address=new Stadium();
+		Stadium totalspace=new Stadium();
+		Stadium fieldspace=new Stadium();
+		Stadium totalseats=new Stadium();
+		Stadium height=new Stadium();
+		Stadium totallobbies=new Stadium();
+		Stadium state=new Stadium();
+	}
+}
